43bd0d0e18d1bd53003c67ae25324c1d3620f760
CuongTL 🐛 <cuong.tl@kiotviet.com>
2025-02-13 15:35:20 +0700

    Update Docusaurus configuration with new URL and organization name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,14 +10,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://prompts.lecuong.me',
+  url: 'https://prompt.lecuong.me',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'awesome-prompt', // Usually your GitHub org/user name.
+  organizationName: 'cuongtl1992', // Usually your GitHub org/user name.
   projectName: 'awesome-prompt', // Usually your repo name.
 
   onBrokenLinks: 'throw',
